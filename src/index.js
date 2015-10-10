a6c5d2eaa2d84724b020f147bf43f651f974c98b
nbuechler <natebuechler@gmail.com>
2015-10-09 21:48:52 -0400

    simple todo app working with hot-reloader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
+import todoApp from './reducers';
 
-React.render(<App />, document.getElementById('root'));
+let store = createStore(todoApp);
+
+let rootElement = document.getElementById('root');
+React.render(
+  // The child must be wrapped in a function
+  // to work around an issue in React 0.13.
+  <Provider store={store}>
+    {() => <App />}
+  </Provider>,
+  rootElement
+);
